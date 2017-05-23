82b7427dbc14a15dee08483a0b26b7d5373860f4
Noah Vito <noah.a.vito@gmail.com>
2017-05-23 16:46:00 -0400

    fully functional for my playlists

--- spotify_playlist_manager.py
+++ spotify_playlist_manager.py
@@ -4,7 +4,41 @@ import sys
 import spotipy
 import spotipy.util as util
 
-scope = 'user-library-read'
+def get_playlist_tracks(current_user_id, playlist_id):
+    playlist_tracks = []
+    offset = 0
+    more_tracks = True
+    while more_tracks:
+        playlist = sp.user_playlist_tracks(current_user_id, playlist_id, limit=100, offset=offset)
+        print(str(offset) + ", " + str(len(playlist['items'])))
+        for item in playlist['items']:
+            track_id = item['track']['id']
+            if track_id is not None:
+                playlist_tracks.append(track_id)
+        offset += 100
+        if len(playlist['items']) != 100:
+            more_tracks = False
+    return playlist_tracks
+
+def get_library_tracks(current_user_id):
+    library_tracks = []
+    offset = 0
+    more_tracks = True
+    while more_tracks:
+        library = sp.current_user_saved_tracks(limit=50, offset=offset)
+        print(str(offset) + ", " + str(len(library['items'])))
+        for item in library['items']:
+            track_id = item['track']['id']
+            if track_id is not None:
+                library_tracks.append(track_id)
+        offset += 50
+        if len(library['items']) != 50:
+            more_tracks = False
+    return library_tracks
+            
+
+
+scope = 'user-library-modify playlist-modify-public'
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -24,9 +58,34 @@ token = util.prompt_for_user_token(username, scope, client_id=credentials['clien
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print track['name'] + ' - ' + track['artists'][0]['name']
+    current_user_id = sp.current_user()['id']
+    playlists = sp.current_user_playlists()
+    for item in playlists['items']:
+        if item['name'] == 'Current Favorites':
+            current_favorites_id = item['id']
+        elif item['name'] == 'Starred':
+            playlist_id = item['id']
+        
+    print("Getting current favorites tracks")
+    current_favorites_tracks = get_playlist_tracks(current_user_id, current_favorites_id)
+            
+    print("Getting starred tracks")
+    starred_tracks = get_playlist_tracks(current_user_id, playlist_id)
+    
+        
+    print("Getting library tracks")
+    library_tracks = get_library_tracks(current_user_id)
+    
+        
+    for track in current_favorites_tracks:
+        if track not in starred_tracks:
+            sp.user_playlist_add_tracks(current_user_id, playlist_id, [track])
+        if track not in library_tracks:
+            sp.current_user_saved_tracks_add([track])
+        
+    
+
+
+
 else:
     print "Can't get token for", username
